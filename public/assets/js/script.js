baa1c741e6dc0713466ef9086a19e16bc8178483
raniabadis00-ai <raniabadis00@gmail.com>
2026-04-07 16:14:56 +0200

    add cookie script

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,15 +1,31 @@
 console.log('hello');
 
+document.cookie = "username=Deschger;";
+document.cookie = "email=email@xss.fr;";
+document.cookie = "password=password;";
+
+
+const main = document.querySelector('main');
+const a = document.createElement('a');
+
+a.textContent = 'Voir les établissements';
+a.href = 'https://youtube.com';
+a.target = '_blank';
+a.rel = 'noopener noreferrer';
+
+a.classList.add('btn', 'btn-danger');
+
+main.append(a);
 
 /** @info
  * A coller dans le input description pour la demo
  * */
 
-// <script>
-//
+// // <script>
+// //
 // const p = document.querySelectorAll('p');
 //
 // for (const e of p) {
 //     e.style.color = 'red';
 // }
-// </script>
+// // </script>
--- template/product/new.php
+++ template/product/new.php
@@ -2,24 +2,24 @@
 
 <form action="" method="post" enctype="multipart/form-data">
 
-    <div>
-        <label for="picture">Image</label>
-        <input id="picture" type="file" name="picture" placeholder="Choisissez une image" accept="image/*">
+    <div class="mb-3">
+        <label for="picture" class="form-label">Image</label>
+        <input id="picture" type="file" name="picture" class="form-control" placeholder="Choisissez une image" accept="image/*">
     </div>
 
-    <div>
-        <label for="title">Titre</label>
-        <input id="title" type="text" name="title" placeholder="Saissisez un titre" required>
+    <div class="mb-3">
+        <label for="title" class="form-label">Titre</label>
+        <input id="title" type="text" name="title" class="form-control" placeholder="Saissisez un titre" required>
     </div>
 
-    <div>
-        <label for="price">Price</label>
-        <input id="price" type="number" name="price" placeholder="Saissisez un prix" required>
+    <div class="mb-3">
+        <label for="price" class="form-label">Price</label>
+        <input id="price" type="number" name="price" class="form-control" placeholder="Saissisez un prix" required>
     </div>
 
-    <div>
-        <label for="description">Description</label>
-        <textarea name="description" id="description" cols="30" rows="10" placeholder="Saisissez une description" style="resize: none;" required></textarea>
+    <div class="mb-3">
+        <label for="description" class="form-label">Description</label>
+        <textarea name="description" id="description" class="form-control" cols="30" rows="10" placeholder="Saisissez une description" style="resize: none;" required></textarea>
     </div>
 
     <button type="submit" class="btn btn-primary">Créer</button>
