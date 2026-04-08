661c52a9ebf196f8319be4a3c76375a371bbe62f
raniabadis00-ai <raniabadis00@gmail.com>
2026-04-08 11:38:29 +0200

    fix: xss reflected

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,7 +9,7 @@ class HomeController extends Controller
 {
     public function index(): void
     {
-        $q = isset($_GET['q']) ? $_GET['q'] : '';
+        $q = filter_input(INPUT_GET, 'q', FILTER_VALIDATE_URL);
 
         $productRepository = new ProductRepository();
         $products = $productRepository->findAll();
